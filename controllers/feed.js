5b60da6a4c7120c6cd9a1b61fcc3794010daa341
Austin Wright <awguitar86@gmail.com>
2020-02-10 15:26:22 -0700

    edited express-validator requiring

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,4 @@
-const { validationResult } = require('express-validator/check')
+const { validationResult } = require('express-validator')
 const Item = require('../models/item')
 
 exports.getItems = (req, res, next) => {
